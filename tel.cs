fc2ed1cd9afb08474bbc71dfc3788c8a5abfbba0
gabriel-diogo <104277959+gabriel-diogo@users.noreply.github.com>
2022-10-27 05:20:51 -0300

    Add files via upload

---
+++ tel.cs
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class tell {
+  static void Main() {
+      
+       
+     for (int i = 0; i < 1000; i++){
+    Random nu = new Random();
+    
+     int val;val = nu.Next(0,9999999);
+    Console.WriteLine("(58)99"+val);
+    //Console.WriteLine("(58)98"+val);
+    
+  }
+      
+  }
+}
