d78e7acc09f0e00816b87e07f9892dbb78107142
gabriel-diogo <104277959+gabriel-diogo@users.noreply.github.com>
2022-10-28 05:12:53 -0300

    Update tel.cs

--- tel.cs
+++ tel.cs
@@ -2,15 +2,16 @@
 
 using System;
 class tell {
-  static void Main() {
-      
-       
+ static void Main() {
+       Random nu = new Random();
+       Random nu1 = new Random();
      for (int i = 0; i < 1000; i++){
-    Random nu = new Random();
+   
     
-     int val;val = nu.Next(0,9999999);
+     int val = nu.Next(9,9999999);
+     int val1 = nu1.Next(9,9999999);
     Console.WriteLine("(58)99"+val);
-    //Console.WriteLine("(58)98"+val);
+    Console.WriteLine("(68)98"+val1);
     
   }
       
